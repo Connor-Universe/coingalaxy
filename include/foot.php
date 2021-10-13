7d805d5970d3ed9939ba4885d7f82e3df4cadac5
Dew Onukwube <58600137+Connor-Universe@users.noreply.github.com>
2021-10-13 21:55:18 +0100

    first commit

---
+++ include/foot.php
@@ -0,0 +1,53 @@
+<footer class="main_footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-12">
+          <div class="footerlogo">
+          <a href="index.php?a=home"><?php echo"$site_name3";?></a>
+            <p>&copy; COPYRIGHT 2019. <?php echo"$site_name3";?>. 
+              ALL RIGHTS RESERVED</p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-12">
+          <div class="footerlinks">
+            <h4>quick links:</h4>
+            <ul>
+              <li><a href="index.php?a=home">HOME</a></li>
+              <li><a href="index3898.php?a=cust&amp;page=about">ABOUT US</a></li>
+              <li><a href="index38cd.php?a=faq">FAQ</a></li>
+            </ul>
+            <ul>
+              <li><a href="index15a0.php?a=support">SUPPORT</a></li>
+              <li><a href="indexa972.php?a=rules">TERMS &amp; CONDITIONS</a></li>
+              <li><a href="index9c68.php?a=cust&amp;page=affiliate">AFFILIATES</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-12">
+          <div class="footerlinks">
+            <h4>contacts:</h4>
+            <p class="footeremail"><?php echo"$email_admin1";?></p>
+            <p class="footerlocation"><?php echo"$location123";?></p>
+        
+               
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</section>
+
+<script>		
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
+<script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="d7923ddb-be7b-483a-93e8-d66cdc86484d";(function(){d=document;s=d.createElement("script");s.src="../client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+
+
+
+
+</body>
+
+<!-- Mirrored from <?php echo"$site_name3";?>/?a=cust&page=howto by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 29 Apr 2021 11:46:25 GMT -->
+</html> 
